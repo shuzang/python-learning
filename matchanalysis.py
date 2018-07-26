c2349508de22aa9984e0073a4e5604ce1d4ecacb
shuzang <844327696@qq.com>
2018-07-26 11:16:36 +0800

    实例：体育竞技比赛分析

---
+++ matchanalysis.py
@@ -0,0 +1,70 @@
+"""
+@title:matchanalysis.py
+@author:shuzang
+@date:2018-7-26 11:14
+@brief:体育竞技比赛分析(这个没写，直接复制过来的)
+"""
+
+# MatchAnalysis.py
+from random import random
+
+
+def printIntro():
+    print("这个程序模拟两个选手A和B的某种竞技比赛")
+    print("程序运行需要A和B的能力值（以0到1之间的小数表示）")
+
+
+def getInputs():
+    a = eval(input("请输入选手A的能力值(0-1): "))
+    b = eval(input("请输入选手B的能力值(0-1): "))
+    n = eval(input("模拟比赛的场次: "))
+    return a, b, n
+
+
+def simNGames(n, probA, probB):
+    winsA, winsB = 0, 0
+    for i in range(n):
+        scoreA, scoreB = simOneGame(probA, probB)
+        if scoreA > scoreB:
+            winsA += 1
+        else:
+            winsB += 1
+    return winsA, winsB
+
+
+def gameOver(a, b):
+    return a == 15 or b == 15
+
+
+def simOneGame(probA, probB):
+    scoreA, scoreB = 0, 0
+    serving = "A"
+    while not gameOver(scoreA, scoreB):
+        if serving == "A":
+            if random() < probA:
+                scoreA += 1
+            else:
+                serving = "B"
+        else:
+            if random() < probB:
+                scoreB += 1
+            else:
+                serving = "A"
+    return scoreA, scoreB
+
+
+def printSummary(winsA, winsB):
+    n = winsA + winsB
+    print("竞技分析开始，共模拟{}场比赛".format(n))
+    print("选手A获胜{}场比赛，占比{:0.1%}".format(winsA, winsA / n))
+    print("选手B获胜{}场比赛，占比{:0.1%}".format(winsB, winsB / n))
+
+
+def main():
+    printIntro()
+    probA, probB, n = getInputs()
+    winsA, winsB = simNGames(n, probA, probB)
+    printSummary(winsA, winsB)
+
+
+main()
